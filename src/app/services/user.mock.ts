57d65e0ec3792cf85a656626fded5819dcecb525
Julia Gonzalez <vanesa_garro@hotmail.com>
2021-12-13 14:56:02 -0300

    usermock

---
+++ src/app/services/user.mock.ts
@@ -0,0 +1,8 @@
+import { User } from '../models/user.model';
+
+export const userMock: User[] = [
+  {nombre:'julia gonzalez', mail:'julia@gmail.com', password: '12345678'},
+  {nombre:'Fiona Ochoa', mail:'fionaa@gmail.com', password: '12345678'},
+  {nombre:'Tommy', mail:'tommy@gmail.com', password: '12345678'},
+  {nombre:'Olivia', mail:'oli@gmail.com', password: '12345678'},
+]
