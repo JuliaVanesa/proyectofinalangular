c1d0174e4cf1d893150ebc53b03c310a52e1b76b
Julia Gonzalez <vanesa_garro@hotmail.com>
2021-12-22 11:21:17 -0300

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MoviesComponent } from './components/movies/movies.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'peliculas',
+    component: MoviesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
